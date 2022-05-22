af16318ab5cad328a84dd2d183a9c581e217e10b
Rafael Nascimento <rafael_freedom@outlook.com>
2022-05-22 08:18:25 -0300

    correções

--- script.js
+++ script.js
@@ -253,6 +253,22 @@ function validar(event) {
     }
 }
 
+function detectarDispositivo() {
+    if (
+        navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        mobile.style.display = "block";
+    } else {
+        mobile.style.display = "none";
+    }
+}
+
 function gambiarra(keyCode) {
     console.log(keyCode);
     if (keyCode >= 97 && keyCode <= 122) {
